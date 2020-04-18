b9058669623853856332c1fa0739cbd0eb6cd562
Peter <peter.ping666@gmail.com>
2020-04-17 20:05:39 -0700

    update web services

--- lyrics-services/src/main/java/com/lyricshub/lyricsservices/WebServices.java
+++ lyrics-services/src/main/java/com/lyricshub/lyricsservices/WebServices.java
@@ -1,6 +1,7 @@
 package com.lyricshub.lyricsservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,20 @@ public class WebServices {
     @Autowired
     LyricsRepo repo;
 
-    @RequestMapping("")
-    public LyricsHub getLricsHub() {
-        return repo.findById(100);
+    @GetMapping("")
+    public LyricsHub getLricsHubList() {
+        return repo.findById(0);
     }
 
+//    @PostMapping("api")
+//    public LyricsHub getLricsHubList() {
+//        return repo.findById(100);
+//    }
+
+    @DeleteMapping("")
+    public void deleteLricsHubList() {
+        repo.deleteById(0);
+    }
 
 }
 
