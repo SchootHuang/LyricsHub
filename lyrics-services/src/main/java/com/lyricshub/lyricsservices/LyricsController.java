cfcb82bb3078ad2b46e184675f575e33ac19fe11
Peter <peter.ping666@gmail.com>
2020-04-29 22:22:56 -0700

    update controller

--- lyrics-services/src/main/java/com/lyricshub/lyricsservices/LyricsController.java
+++ lyrics-services/src/main/java/com/lyricshub/lyricsservices/LyricsController.java
@@ -3,6 +3,7 @@ package com.lyricshub.lyricsservices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,13 +11,12 @@ public class LyricsController {
     @Autowired
     LyricsRepo repo;
 
-    @GetMapping("")
-    public LyricsHub getLricsHubList() {
-        return repo.findById(0);
+    @GetMapping("/{id}")
+    public LyricsHub getByArtists(@PathVariable int id) {
+        return repo.findById(id);
     }
 
-    @DeleteMapping("")
-    public void deleteLricsHubList() {
-        repo.deleteById(0);
+    @GetMapping("/{artists}")
+    public void findByArtists(@PathVariable String artists) {
     }
 }
